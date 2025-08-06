0865f40ce889ff20eb692e18fe3da0f9b56ef06d
𝓐𝓷𝔂_𝓛𝓲𝓵𝓲𝓬𝓪_𝓡𝓲𝓹𝓲𝓵𝓲𝓬𝓪🎀 <alves.silva.ana0507@escola.pr.gov.br>
2025-08-06 09:49:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,10 @@
+criaCartao(
+    "Biologia",
+    "Qual o reino das plantas?",
+    "Reino Plantae"
+)
+criaCartao(
+    "Biologia",
+    "Quem faz parte dos vertebrados?",
+    "Aves,Mamíferos,Répteis e Anfíbios",
+)
