f4989dfadf8a34f6de36dfcb21176ee1cc600a03
𝓐𝓷𝔂_𝓛𝓲𝓵𝓲𝓬𝓪_𝓡𝓲𝓹𝓲𝓵𝓲𝓬𝓪🎀 <alves.silva.ana0507@escola.pr.gov.br>
2025-08-08 09:50:22 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,10 +1,30 @@
 criaCartao(
-    "Biologia",
-    "Qual o reino das plantas?",
-    "Reino Plantae"
+    "Geografia",
+    "1- Qual é o país conhecido como a terra do sol nascente?",
+    "Japão"
 )
 criaCartao(
-    "Biologia",
-    "Quem faz parte dos vertebrados?",
-    "Aves,Mamíferos,Répteis e Anfíbios",
+    "Geografia",
+    "2- Quantos continentes existem no mundo?",
+    "Sete, África, América do Norte, América do Sul, Antártida, Ásia, Europa e Oceania",
+)
+criaCartao(
+    "Geografia",
+    "3- Qual é o menor país do mundo?",
+    "Vaticano",
+)
+criaCartao(
+    "Geografia",
+    "4- Qual é o continente com a maior diversidade de animais?",
+    "África",
+)
+criaCartao(
+    "Geografia",
+    "5- O que é o efeito estufa?",
+    "O processo natural de aquecimento da Terra devido à retenção de calor pelos gases da atmosfera",
+)
+criaCartao(
+    "Geografia",
+    "6- Qual é o ponto mais alto da Terra?",
+    "Monte Everest",
 )
